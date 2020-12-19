a9da0b087e29447c30bfc304ee00049184c3dd0e
Siddharth Cherukupalli <cherukusid@gmail.com>
2020-12-19 00:58:02 -0800

    fixed firebase

---
+++ FinanceApp/FinanceApp/Volunteer/AvailableRestTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AvailableRestTableViewCell.swift
+//  FinanceApp
+//
+//  Created by Siddharth on 12/18/20.
+//
+
+import UIKit
+
+class AvailableRestTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var restName: UILabel!
+    @IBOutlet weak var hospName: UILabel!
+    
+}
