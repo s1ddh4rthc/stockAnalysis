33bf965441c2cd5ac97ffe82fffb3ea91d0cdeb2
Arth Bohra <arthbohra@gmail.com>
2020-12-18 19:23:20 -0800

    added view controllers

--- FinanceApp/FinanceApp/signup.swift
+++ FinanceApp/FinanceApp/signup.swift
@@ -13,6 +13,7 @@ class signup: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet var posPicker: UISegmentedControl!
     
     func errorMessage(title: String, message: String) {
         
@@ -45,6 +46,134 @@ class signup: UIViewController, UITextFieldDelegate {
             }
             
         }
+        guard let emailLolz = email.text as? String else { return };
+        
+        if posPicker.selectedSegmentIndex == 0 {
+            
+            var values = ["email": emailLolz
+                
+                
+                ]] as [String : Any]
+            var user = Auth.auth().currentUser
+            if let user = user {
+                let uid = user.uid
+                Database.database().reference().child("volunteers").child(uid).updateChildValues(values,withCompletionBlock: {(error, ref) in
+                    if let error = error {
+                        print ("Failed to update database value with errors: ", error.localizedDescription)
+                        
+                        return
+                    }
+                    
+                })
+            }
+            
+        }
+        if posPicker.selectedSegmentIndex == 1 {
+            
+            var values = ["email": emailLolz
+                
+                
+                ]] as [String : Any]
+            var user = Auth.auth().currentUser
+            if let user = user {
+                let uid = user.uid
+                Database.database().reference().child("employee").child(uid).updateChildValues(values,withCompletionBlock: {(error, ref) in
+                    if let error = error {
+                        print ("Failed to update database value with errors: ", error.localizedDescription)
+                        
+                        return
+                    }
+                    
+                })
+            }
+            
+        }
+        if posPicker.selectedSegmentIndex == 2 {
+            
+            var values = ["email": emailLolz
+                
+                
+                ]] as [String : Any]
+            var user = Auth.auth().currentUser
+            if let user = user {
+                let uid = user.uid
+                Database.database().reference().child("employer").child(uid).updateChildValues(values,withCompletionBlock: {(error, ref) in
+                    if let error = error {
+                        print ("Failed to update database value with errors: ", error.localizedDescription)
+                        
+                        return
+                    }
+                    
+                })
+            }
+            
+        }
+        if posPicker.selectedSegmentIndex == 3 {
+            
+            var values = ["email": emailLolz
+                
+                
+                ]] as [String : Any]
+            var user = Auth.auth().currentUser
+            if let user = user {
+                let uid = user.uid
+                Database.database().reference().child("bank").child(uid).updateChildValues(values,withCompletionBlock: {(error, ref) in
+                    if let error = error {
+                        print ("Failed to update database value with errors: ", error.localizedDescription)
+                        
+                        return
+                    }
+                    
+                })
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        let uid = Auth.auth().currentUser?.uid
+        
+        
+        let  values = ["lastName": lastName.text, "name": firstName.text, "teamName": nameTeam.text, "school": school.text
+            ] as [String : Any]
+        
+        
+        
+        let ref = Database.database().reference()
+        ref.child("someid/" + String(uid!) + "/teamName").observeSingleEvent(of: .value) { (snapshot) in
+            guard let coachUID = snapshot.value as? String else { return }
+            Database.database().reference().child("coaches/" + coachUID + "/players").child(uid ?? " ").updateChildValues(values,withCompletionBlock: {(error, ref) in
+                        if let error = error {
+                            print ("Failed to update database value with errors: ", error.localizedDescription)
+                            DispatchQueue.main.async {
+                                let navController = UINavigationController(rootViewController: Homepage())
+                                
+                            }
+                            return
+                        }
+                        Database.database().reference().child("someid").child(String(uid!)).updateChildValues(values,withCompletionBlock: {(error, ref) in
+                            if let error = error {
+                                print ("Failed to update database value with errors: ", error.localizedDescription)
+                                DispatchQueue.main.async {
+                                    let navController = UINavigationController(rootViewController: Homepage())
+                                    
+                                }
+                                
+                            }
+                          
+                            
+                            
+                            
+                        })
+                }
+                
+                
+            )}
         
         
     }
