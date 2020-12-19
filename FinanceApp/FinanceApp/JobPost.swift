c2a14268c5e4141f565b5789c20070a9673fead8
Arth Bohra <arthbohra@gmail.com>
2020-12-18 17:11:29 -0800

    added view controllers

---
+++ FinanceApp/FinanceApp/JobPost.swift
@@ -0,0 +1,29 @@
+//
+//  JobPost.swift
+//  FinanceApp
+//
+//  Created by Arth Bohra on 12/18/20.
+//
+
+import UIKit
+
+class JobPost: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
