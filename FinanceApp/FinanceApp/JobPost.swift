f6a429fd12c2b86766448af1899ddc28c5a5a619
Arth Bohra <arthbohra@gmail.com>
2020-12-18 20:35:24 -0800

    added view controllers

--- FinanceApp/FinanceApp/JobPost.swift
+++ FinanceApp/FinanceApp/JobPost.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class JobPost: UIViewController {
 
+    @IBOutlet var jobTitle: UITextField!
+    @IBOutlet var necessarySkills: UITextField!
+    @IBOutlet var employerName: UITextField!
+    @IBOutlet var jobLocation: UITextField!
+    @IBOutlet var otherRequirements: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func postJob(_ sender: Any) {
+        guard let post = jobTitle.text as? String else { return }
+        guard let employName = employerName.text as? String else { return }
+        guard let necSkills = necessarySkills.text as? String else { return }
+        guard let jobLocate = jobLocation.text as? String else { return }
+        guard let otherStuffZ = otherRequirements.text as? String else { return }
+        let user = Auth.auth().currentUser
+        guard let uid =  (user?.uid)! as? String else { return }
+        if let user = user {
+            let uid = user.uid
+            let postRef = Database.database().reference().child("users/" + (uid) + "/posts").childByAutoId()
+            let postRef1 = Database.database().reference().child("jobPosts").childByAutoId()
+            
+            let postObject = [
+                
+                "title": post,
+                "skills": necSkills,
+                "name": emplyName,
+                "location": jobLocate,
+                "other": otherStuffZ,
+                "timestamp": [".sv": "timestamp"]
+                
+                ] as [String: Any]
+            
+            postRef.setValue(postObject) { (error, ref) in
+                if error == nil {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print ("Error", error?.localizedDescription)
+                }
+            
+          
+        }
+            postRef1.setValue(postObject) { (error, ref) in
+                if error == nil {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print ("Error", error?.localizedDescription)
+                }
+            
+        }
+        
+        
+        
+            
+        }
+        
+    }
+
     
 
     /*
